b85387fcb100eae042279ea4c9505591e8d9bf01
Derrick Roccka <derrickroccka@gmail.com>
2017-05-02 14:22:51 +0200

    Added webpack, index.js, web pack dev server and index.html

---
+++ index.js
@@ -0,0 +1,21 @@
+// 'use strict';
+//
+//
+//
+// (function(){
+// 	console.log('Derrick Roccka');
+// 	var burst = new Burst({
+// 		radius:   { 15: 50 },
+// 		children: {
+// 			fill:   [ 'deeppink', 'cyan', 'orange' ]
+// 		}
+// 	});
+//
+// 	document.getElementById('button').addEventListener( 'click', function (e) {
+// 		console.log('e', e);
+// 		burst
+// 			.tune({ x: e.pageX, y: e.pageY })
+// 			.generate()
+// 			.replay();
+// 	});
+// })();
